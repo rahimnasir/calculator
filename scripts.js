0f9531e008f6b30a43d3f3a4e96abc707191ac85
rahimnasir <68578449+rahimnasir@users.noreply.github.com>
2024-01-14 14:42:47 +0800

    Finished?

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 let operatorSelected = false
-
+let equationExecuted = false
 let buttonValue1 =""
 let buttonValue2 =""
 let operatorSign =""
 
 let equationText =""
 
+let result = 0
+let num1 = 0
+let num2 = 0
 function clearAll(){
     operatorSelected = false
 
@@ -42,40 +45,53 @@ function insertNum(button){
 
 
 function insertOperator(button){
-    if(operatorSelected == true){
-        console.log(executeOperation().num1,executeOperation().result)
-        executeOperation().num1 = executeOperation().result
-        executeOperation().result = 0
+    if(operatorSelected == true && buttonValue2 != 0){
+        buttonValue1 = result
+        buttonValue2 = ""
+        result = 0
+        updateEquation()
         operatorSelected == false
+        equationExecuted = false
     }
+
+
+    
     operatorSign = button.innerHTML
     updateEquation()
     operatorSelected=true
     console.log(operatorSign)
 }
 
+
 function executeOperation(){
-    
-    let result = 0
-    let num1 = parseInt(buttonValue1)
-    let num2 = parseInt(buttonValue2)
+    if(operatorSelected == true){
+        num1 = parseInt(buttonValue1)
+        num2 = parseInt(buttonValue2)
 
 
-    if(operatorSign === "+"){
-        result = num1+num2
-    }else if(operatorSign === "-"){
-        result = num1-num2
-    }else if(operatorSign === "*"){
-        result = num1*num2
-    }else if(operatorSign === "/"){
-        if(num2 != 0){
-            result = num1/num2
-        }else if(num2 == 0){
-            result = "ERROR!"
+        if(operatorSign === "+"){
+            result = Math.round(num1+num2)
+        }else if(operatorSign === "-"){
+            result = Math.round(num1-num2)
+        }else if(operatorSign === "*"){
+            result = Math.round(num1*num2)
+        }else if(operatorSign === "/"){
+            if(num2 != 0){
+                result = Math.round(num1/num2)
+            }else if(num2 == 0){
+                result = "ERROR!"
+            }
         }
+        
+        if(equationExecuted == false){
+            console.log("Result: "+result)
+            document.getElementById("equation").innerHTML += ("="+String(result))
+            equationExecuted = true
+        }
+    }else if(operatorSelected == false){
+
     }
-    console.log("Result: "+result)
-    document.getElementById("equation").innerHTML += ("="+String(result))
+    
     
 }
 
